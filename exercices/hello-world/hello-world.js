f72b7a85ce05d694abe6eb9432262f59df598a32
Ryan Läuppi <pa33xxl@eduvaud.ch>
2025-11-07 14:02:09 +0100

    feat(HelloWorld) : Terminer l'exercice

--- exercices/hello-world/hello-world.js
+++ exercices/hello-world/hello-world.js
@@ -3,6 +3,9 @@
 // Il t’est fourni pour te permettre de démarrer plus rapidement l’écriture du code.
 //
 
+let msg = "Hello, World!"
+
 export function hello() {
-  throw new Error("Remove this line and implement the function");
+  return msg
 }
+

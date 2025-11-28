eb3167b0428d484ef331ab0e3eb2b206d53af456
Ryan Läuppi <pa33xxl@eduvaud.ch>
2025-11-28 13:47:36 +0100

    feat(space-age):Finir l'exercice
    [55][Done]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,38 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, ageInSeconds) => {
+  if (planet === "earth") {
+    return Number((ageInSeconds / 31557600).toFixed(2));
+  } else if (planet === "mercury") {
+    return Number(
+      (ageInSeconds / (365.25 * 0.2408467 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else if (planet === "venus") {
+    return Number(
+      (ageInSeconds / (365.25 * 0.61519726 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else if (planet === "mars") {
+    return Number(
+      (ageInSeconds / (365.25 * 1.8808158 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else if (planet === "jupiter") {
+    return Number(
+      (ageInSeconds / (365.25 * 11.862615 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else if (planet === "saturn") {
+    return Number(
+      (ageInSeconds / (365.25 * 29.447498 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else if (planet === "uranus") {
+    return Number(
+      (ageInSeconds / (365.25 * 84.016846 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else if (planet === "neptune") {
+    return Number(
+      (ageInSeconds / (365.25 * 164.79132 * 60 * 60 * 24)).toFixed(2)
+    );
+  } else {
+    throw new Error("not a planet");
+  }
 };

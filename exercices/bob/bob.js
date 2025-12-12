2a9321d44168ebd43c7eb62713f67c7ba9a6ed56
Ryan Läuppi <pa33xxl@eduvaud.ch>
2025-12-12 16:26:55 +0100

    feat(bob):finir l'exercice
    [75][Done]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -4,5 +4,21 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+  if (message.trim().endsWith("?")) {
+    if (/[A-Z]/.test(message) && !/[a-z]/.test(message)) {
+      return "Calm down, I know what I'm doing!";
+    }
+    return "Sure.";
+  } else if (message.match(/^[A-Z]+$/)) {
+    return "Whoa, chill out!";
+  }
+  if (/[a-zA-Z\d]/.test(message)) {
+    if (!/[a-z]/.test(message) && /[A-Z]/.test(message)) {
+      return "Whoa, chill out!";
+    } else {
+      return "Whatever.";
+    }
+  } else {
+    return "Fine. Be that way!";
+  }
 };

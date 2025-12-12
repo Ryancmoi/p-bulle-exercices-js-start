fb58ad7e48c681ca03d8ba8d97e76a0adcc8d919
Ryan Läuppi <pa33xxl@eduvaud.ch>
2025-12-12 16:33:27 +0100

    feat(lucky-numbers):Commencer l'exercice
    [10][En cours]

--- exercices/lucky-numbers/lucky-numbers.js
+++ exercices/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,8 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  let array1Numbers = "";
+  for (let i = 0; i < array1.length; i++) {}
 }
 
 /**
@@ -18,7 +19,7 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+  throw new Error("Remove this line and implement the function");
 }
 
 /**
@@ -29,5 +30,5 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+  throw new Error("Remove this line and implement the function");
 }

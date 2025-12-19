9f3ebeca4b40a6e25dd218e3c864bacad1aed5f0
Ryan Läuppi <pa33xxl@eduvaud.ch>
2025-12-19 16:06:41 +0100

    feat(resistor-color):Finir l'exercice
    [5][Done]

--- exercices/resistor-color/resistor-color.js
+++ exercices/resistor-color/resistor-color.js
@@ -3,8 +3,48 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error('Remove this line and implement the function');
+export const colorCode = (color) => {
+  switch (color) {
+    case "black":
+      return 0;
+    case "brown":
+      return 1;
+
+    case "red":
+      return 1;
+
+    case "orange":
+      return 3;
+
+    case "yellow":
+      return 4;
+
+    case "green":
+      return 5;
+
+    case "blue":
+      return 6;
+
+    case "violet":
+      return 7;
+
+    case "grey":
+      return 8;
+
+    case "white":
+      return 9;
+  }
 };
 
-export const COLORS = undefined;
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
